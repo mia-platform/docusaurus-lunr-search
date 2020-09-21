ff200a790c71d7c2114fdc5ac15190ae91930f0c
Riccardo Zambito <riccardozambito@mia-platform.eu>
2020-09-21 12:23:29 +0200

    Fix lint and versioned search implemented

--- src/theme/SearchBar/lib/lunar-search.js
+++ src/theme/SearchBar/lib/lunar-search.js
@@ -7,14 +7,20 @@ class LunrSearchAdapter {
         this.lunrIndex = lunr.Index.load(searchIndex);
     }
 
-    getLunrResult(input) {
+    getLunrResult(input, versions) {
         return this.lunrIndex.query(function (query) {
             const tokens = lunr.tokenizer(input);
+
             query.term(tokens, {
-                boost: 10
+                boost: 10,
             });
             query.term(tokens, {
-                wildcard: lunr.Query.wildcard.TRAILING
+                wildcard: lunr.Query.wildcard.TRAILING,
+            });
+            query.term(versions.join(" "), {
+                fields: ["version"],
+                boost: 0,
+                presence: lunr.Query.presence.REQUIRED,
             });
         });
     }
@@ -23,42 +29,53 @@ class LunrSearchAdapter {
         return {
             hierarchy: {
                 lvl0: doc.pageTitle || doc.title,
-                lvl1: doc.type === 0 ? null : doc.title
+                lvl1: doc.type === 0 ? null : doc.title,
             },
             url: doc.url,
             _snippetResult: formattedContent ? {
                 content: {
                     value: formattedContent,
-                    matchLevel: "full"
-                }
+                    matchLevel: "full",
+                },
             } : null,
             _highlightResult: {
                 hierarchy: {
                     lvl0: {
                         value: doc.type === 0 ? formattedTitle || doc.title : doc.pageTitle,
                     },
-                    lvl1:
-                        doc.type === 0
-                            ? null
-                            : {
-                                value: formattedTitle || doc.title
-                            }
-                }
-            }
+                    lvl1: doc.type === 0 ?
+                        null : {
+                            value: formattedTitle || doc.title,
+                        },
+                },
+            },
         };
     }
     getTitleHit(doc, position, length) {
         const start = position[0];
         const end = position[0] + length;
-        let formattedTitle = doc.title.substring(0, start) + '<span class="algolia-docsearch-suggestion--highlight">' + doc.title.substring(start, end) + '</span>' + doc.title.substring(end, doc.title.length);
-        return this.getHit(doc, formattedTitle)
+        let formattedTitle =
+            doc.title.substring(0, start) +
+            '<span class="algolia-docsearch-suggestion--highlight">' +
+            doc.title.substring(start, end) +
+            "</span>" +
+            doc.title.substring(end, doc.title.length);
+        return this.getHit(doc, formattedTitle);
     }
 
     getKeywordHit(doc, position, length) {
         const start = position[0];
         const end = position[0] + length;
-        let formattedTitle = doc.title + '<br /><i>Keywords: ' + doc.keywords.substring(0, start) + '<span class="algolia-docsearch-suggestion--highlight">' + doc.keywords.substring(start, end) + '</span>' + doc.keywords.substring(end, doc.keywords.length) + '</i>'
-        return this.getHit(doc, formattedTitle)
+        let formattedTitle =
+            doc.title +
+            "<br /><i>Keywords: " +
+            doc.keywords.substring(0, start) +
+            '<span class="algolia-docsearch-suggestion--highlight">' +
+            doc.keywords.substring(start, end) +
+            "</span>" +
+            doc.keywords.substring(end, doc.keywords.length) +
+            "</i>";
+        return this.getHit(doc, formattedTitle);
     }
 
     getContentHit(doc, position) {
@@ -69,9 +86,9 @@ class LunrSearchAdapter {
         let ellipsesBefore = true;
         let ellipsesAfter = true;
         for (let k = 0; k < 3; k++) {
-            const nextSpace = doc.content.lastIndexOf(' ', previewStart - 2);
-            const nextDot = doc.content.lastIndexOf('.', previewStart - 2);
-            if ((nextDot > 0) && (nextDot > nextSpace)) {
+            const nextSpace = doc.content.lastIndexOf(" ", previewStart - 2);
+            const nextDot = doc.content.lastIndexOf(".", previewStart - 2);
+            if (nextDot > 0 && nextDot > nextSpace) {
                 previewStart = nextDot + 1;
                 ellipsesBefore = false;
                 break;
@@ -84,9 +101,9 @@ class LunrSearchAdapter {
             previewStart = nextSpace + 1;
         }
         for (let k = 0; k < 10; k++) {
-            const nextSpace = doc.content.indexOf(' ', previewEnd + 1);
-            const nextDot = doc.content.indexOf('.', previewEnd + 1);
-            if ((nextDot > 0) && (nextDot < nextSpace)) {
+            const nextSpace = doc.content.indexOf(" ", previewEnd + 1);
+            const nextDot = doc.content.indexOf(".", previewEnd + 1);
+            if (nextDot > 0 && nextDot < nextSpace) {
                 previewEnd = nextDot;
                 ellipsesAfter = false;
                 break;
@@ -100,38 +117,42 @@ class LunrSearchAdapter {
         }
         let preview = doc.content.substring(previewStart, start);
         if (ellipsesBefore) {
-            preview = '... ' + preview;
+            preview = "... " + preview;
         }
-        preview += '<span class="algolia-docsearch-suggestion--highlight">' + doc.content.substring(start, end) + '</span>';
+        preview +=
+            '<span class="algolia-docsearch-suggestion--highlight">' +
+            doc.content.substring(start, end) +
+            "</span>";
         preview += doc.content.substring(end, previewEnd);
         if (ellipsesAfter) {
-            preview += ' ...';
+            preview += " ...";
         }
         return this.getHit(doc, null, preview);
-
     }
-    search(input) {
+    search(input, versions) {
         return new Promise((resolve, rej) => {
-            const results = this.getLunrResult(input);
+            const results = this.getLunrResult(input, versions);
             const hits = [];
             results.length > 5 && (results.length = 5);
-            this.titleHitsRes = []
-            this.contentHitsRes = []
-            results.forEach(result => {
+            this.titleHitsRes = [];
+            this.contentHitsRes = [];
+            results.forEach((result) => {
                 const doc = this.searchDocs[result.ref];
-                const { metadata } = result.matchData;
+                const {
+                    metadata
+                } = result.matchData;
                 for (let i in metadata) {
                     if (metadata[i].title) {
                         if (!this.titleHitsRes.includes(result.ref)) {
-                            const position = metadata[i].title.position[0]
+                            const position = metadata[i].title.position[0];
                             hits.push(this.getTitleHit(doc, position, input.length));
                             this.titleHitsRes.push(result.ref);
                         }
                     } else if (metadata[i].content) {
-                        const position = metadata[i].content.position[0]
-                        hits.push(this.getContentHit(doc, position))
+                        const position = metadata[i].content.position[0];
+                        hits.push(this.getContentHit(doc, position));
                     } else if (metadata[i].keywords) {
-                        const position = metadata[i].keywords.position[0]
+                        const position = metadata[i].keywords.position[0];
                         hits.push(this.getKeywordHit(doc, position, input.length));
                         this.titleHitsRes.push(result.ref);
                     }
